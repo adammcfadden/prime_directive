d38a819f543bc51eab84041cabba681c5cf89d30
Ian MacDonald & Adam McFadden <student@epicodus.com>
2015-05-27 16:58:34 -0700

    non-working code, refactoring to push only prime numbers into array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,32 @@
 var sieve = function(inputNumber) {
-  var numberArray = []
+  var numberArray = [];
 
+  var isPrime = false;
+
+  for(var x=2; x <= inputNumber; x++){
+    for(var checkAgainst = x; checkAgainst > 1; checkAgainst--){
+      if ((x % checkAgainst !== 0) || (x === checkAgainst)){
+        isPrime = true;
+      } else {
+        isPrime = false;
+      }
+      //   isPrime = true;
+      // }
+      // if (x % checkAgainst !== 0){
+      //   isPrime = true;
+      // } else {
+      //   isPrime = false;
+      // }
+
+    }
+    if (isPrime) {
+      numberArray.push(x);
+      isPrime = false;
+    }
+  }
+  debugger;
+  return numberArray;
+};
 
 
 
@@ -9,35 +35,35 @@ var sieve = function(inputNumber) {
   //   numberArray.push(x)
   // }
   // var numberCheck = 2;
-  for(var numberCheck = 0; numberCheck < inputNumber - 2;) {
-    for(var index=2; index < inputNumber; index++) {
-      // for(var location=0; location < numberArray.length; location++)
-      // if (alreadyIncluded(numberCheck, numberArray)) {
-      if (!(index % (numberCheck + 2) === 0)) {
-        numberArray.push(index);
-      } else if (index === (numberCheck + 2)) {
-        numberArray.push(index);
-        // numberCheck++;
-      }
-    }
-    numberCheck = numberArray[((numberCheck) + 1)];
-  }
-  debugger;
-  return numberArray
-  // for(var location=0; location < inputNumber-2; location++) {
-  //   if(numberArray[location] !== null) {
-  //     primeArray.push(numberArray[location]);
-  //   }
-  // }
-  // return primeArray
-};
-
-var alreadyIncluded = function(number, array) {
-  result = false;
-  for(var location = 0; location < array.length; location++) {
-    if (array[location] === number) {
-      result = true;
-    }
-  }
-  return result;
-};
+//   for(var numberCheck = 2; numberCheck < inputNumber;) {
+//     for(var index=2; index < inputNumber; index++) {
+//       // for(var location=0; location < numberArray.length; location++)
+//       // if (alreadyIncluded(numberCheck, numberArray)) {
+//       if (!(index % numberCheck === 0)) {
+//         numberArray.push(index);
+//       } else if (index === numberCheck) {
+//         numberArray.push(index);
+//         // numberCheck++;
+//       }
+//     }
+//     numberCheck = numberArray.length + 2;
+//   }
+//   debugger;
+//   return numberArray
+//   // for(var location=0; location < inputNumber-2; location++) {
+//   //   if(numberArray[location] !== null) {
+//   //     primeArray.push(numberArray[location]);
+//   //   }
+//   // }
+//   // return primeArray
+// };
+//
+// var alreadyIncluded = function(number, array) {
+//   result = false;
+//   for(var location = 0; location < array.length; location++) {
+//     if (array[location] === number) {
+//       result = true;
+//     }
+//   }
+//   return result;
+// };
